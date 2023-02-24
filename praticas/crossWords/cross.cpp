8e80572592140f14b7d82157b07d567e8cb03545
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2023-02-24 16:11:17 -0300

    continuing crosswords

--- praticas/crossWords/cross.cpp
+++ praticas/crossWords/cross.cpp
@@ -4,19 +4,26 @@ using namespace std;
 
 FILE *in;
 
+typedef struct{
+    string word;
+    int position;
+} Word;
+
 class CrossWord {
 
     public:
     int x;
     int y;
+    int position;
     char cross[100][100];
-    vector<string> horizontals;
-    vector<string> verticals;
+    vector<Word> horizontals;
+    vector<Word> verticals;
 
     public:
     CrossWord(int x, int y){
         this->x = x;
         this->y = y;
+        this->position = 0;
     }
 
     void scanWords(){
@@ -26,6 +33,7 @@ class CrossWord {
             }
         }
     }
+
     void findRightWord(int x, int y){
         cout << "FINDING RIGHT " << endl;
         int i = x;
@@ -38,15 +46,18 @@ class CrossWord {
             c++;
         }
         word[c] = '\0';
-        cout << word;
-        this->horizontals.push_back(string(word));
+        Word structWord;
+        structWord.word = string(word);
+        cout << structWord.word << endl;
+        structWord.position = this->position;
+        this->horizontals.push_back(structWord);
     }
 
     void findDownWord(int x, int y){
         cout << "FINDING DOWN " << endl;
         int i = x;
         int j = y;
-        int c = 0;
+        int c = 0;  
         char word [100];
         while(x <= this->x && this->cross[i][j] != '*'){
             word[c] = this->cross[i][j];
@@ -54,33 +65,20 @@ class CrossWord {
             c++;
         }
         word[c] = '\0';
-        cout << word;
-        this->verticals.push_back(string(word));
+        Word structWord;
+        structWord.word = string(word);
+        cout << structWord.word << endl;
+        structWord.position = this->position;
+        this->verticals.push_back(structWord);
     }
 
     void verifyNumeralSquare(int x, int y){
-        cout << x << " " << y;
+        cout << x << y << endl;
         if(this->cross[x][y] != '*'){
-            if(y-1 < 0){
-                if(this->cross[x][y+1] != '*'){
-                    this->findRightWord(x, y);
-                }
-            } else {
-                if(this->cross[x][y - 1] == '*'){
-                    if(y+1 != '*'){
-                        this->findRightWord(x, y);
-                    }
-                }
-            }
-            if(x-1 < this->x){
-                if(this->cross[x+1][y] != '*'){
-                    this->findDownWord(x, y);
-                }
-            } else {
-                if(this->cross[x-1][y] == '*'){
-                    this->findDownWord(x, y);
-                }
-            }
+            if(y-1 < 0 && (this->cross[x][y+1] != '*')) this->findRightWord(x, y);
+            if(this->cross[x][y-1] == '*' && (this->cross[x][y+1] != '*')) this->findRightWord(x ,y);
+            if(x-1 < 0 && (this->cross[x+1][y] != '*')) this->findDownWord(x, y);
+            if(this->cross[x-1][y] == '*' && (this->cross[x+1][y] != '*')) this->findDownWord(x, y);
         }
     }
 
@@ -91,15 +89,17 @@ class CrossWord {
     }
 
     void printHorizontals(){
-        cout << this->horizontals.size();
-        for(int i = this->horizontals.size() - 1; i > 0; i--){
-            cout << horizontals[i];
+        cout << "Printing horizontals " << endl;
+        cout << this->horizontals.size() << endl;
+        for(int i = 0; i < this->horizontals.size(); i++){
+            cout << this->horizontals[i].position << endl << this->horizontals[i].word << endl;
         }
     }
     void printVerticals(){
-        cout << this->verticals.size();
-        for(int i = this->verticals.size() - 1; i > 0; i--){
-            cout << verticals[i];
+        cout << "Printing verticals " << endl;
+        cout << this->verticals.size() << endl;
+        for(int i = 0; i < this->verticals.size(); i++){
+            cout << this->horizontals[i].position << endl << this->horizontals[i].word << endl;
         }
     }
 };
@@ -121,6 +121,7 @@ int main(){
         crossword.scanWords();
         //crossword.printCross();
         //cout << crossword.horizontals[0];
+        cout << "=-=-=-=-=-=-=-=-=-=-=" << endl;
         crossword.printHorizontals();
         crossword.printVerticals();
 
